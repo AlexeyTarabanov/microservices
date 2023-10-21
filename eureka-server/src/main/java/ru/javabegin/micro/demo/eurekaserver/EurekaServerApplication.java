d055a6b481a94204804f5b1f6d02982f12d5b3fe
Alexey Tarabanov <tarabanov@bk.ru>
2023-10-21 19:42:40 +0300

    добавил api gateway

--- eureka-server/src/main/java/ru/javabegin/micro/demo/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/ru/javabegin/micro/demo/eurekaserver/EurekaServerApplication.java
@@ -7,8 +7,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
-// сервер - контейнер для МС
-// клиент - поставщик МС, который будет опубликовывать свои МС на сервер
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        // сервер - контейнер для МС
+        // клиент - поставщик МС, который будет опубликовывать свои МС на сервер
+        // api - переадресация запрсов с уникального модуля на конкретные МС
         System.out.println("Hello world!");
     }
 }
